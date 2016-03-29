cd2a618cacf4929f2ece676457ae47f68d6c4cea
josungil58 <josungil58@gmail.com>
2016-03-29 17:37:04 +0900

    sunshine 2.01 complete, apikey OK

--- Sunshine-Version-2-1.01_hello_world/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Sunshine-Version-2-1.01_hello_world/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine.app;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,10 +25,10 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
-/**
- * onCreate(Bundle savedInstanceState), getSupportFragmentManager
- * MainActivity가 class로 지정되어 있음
- */
+    /**
+     * onCreate(Bundle savedInstanceState), getSupportFragmentManager
+     * MainActivity가 class로 지정되어 있음
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +44,7 @@ public class MainActivity extends ActionBarActivity {
             getSupportFragmentManager().beginTransaction()
                     //beginTransaction()은 activity state저장전에 사용
                     .add(R.id.container, new PlaceholderFragment())
-                    //FragmentTransaction class 참조
+                            //FragmentTransaction class 참조
                     .commit();
         }
     }
@@ -71,6 +79,7 @@ public class MainActivity extends ActionBarActivity {
     public static class PlaceholderFragment extends Fragment {//PlaceholderFragment도 fragment
 
         ArrayAdapter<String> mForecastAdapter;
+
         public PlaceholderFragment() {
         }
 
@@ -106,6 +115,76 @@ public class MainActivity extends ActionBarActivity {
             ListView listView = (ListView) rootView.findViewById(
                     R.id.listview_forecast);//fragment_main.xml listview id
             listView.setAdapter(mForecastAdapter);//sets the data behind this ListView
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are avaiable at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+                String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(apiKey));
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                // Nothing to do.
+                return null;
+
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+
+                }
+                forecastJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+
+            } finally {
+                if (urlConnection != null) {
+                urlConnection.disconnect();
+
+                }
+                if (reader != null) {
+                try {
+                reader.close();
+
+                    } catch (final IOException e) {
+                Log.e("PlaceholderFragment", "Error closing stream", e);
+
+                    }
+
+                }
+
+            }
 
             return rootView;
         }
