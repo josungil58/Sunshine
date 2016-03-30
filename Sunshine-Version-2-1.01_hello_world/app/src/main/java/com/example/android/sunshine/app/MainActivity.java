689f7f6585198f788b0306c5cb5cdeb950394c76
josungil58 <josungil58@gmail.com>
2016-03-30 09:10:13 +0900

    작업중 이동

--- Sunshine-Version-2-1.01_hello_world/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Sunshine-Version-2-1.01_hello_world/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -119,16 +119,21 @@ public class MainActivity extends ActionBarActivity {
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
+            //urlConnection class 상속
+            //null은 연결하지 않겠다는 의미로 추정
             BufferedReader reader = null;
+            //data reading using buffer
 
             // Will contain the raw JSON response as a string.
             String forecastJsonStr = null;
+            //아직은 연결하지 않겠다는 의미 - null, 장래 활용
 
             try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are avaiable at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
                 String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+                //날씨 data 7개를 받아서 string형식으로 저장
                 String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
                 URL url = new URL(baseUrl.concat(apiKey));
 
@@ -141,23 +146,23 @@ public class MainActivity extends ActionBarActivity {
                 InputStream inputStream = urlConnection.getInputStream();
                 StringBuffer buffer = new StringBuffer();
                 if (inputStream == null) {
-                // Nothing to do.
-                return null;
+                    // Nothing to do.
+                    return null;
 
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
 
                 String line;
                 while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line + "\n");
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
                 }
 
                 if (buffer.length() == 0) {
-                // Stream was empty.  No point in parsing.
-                return null;
+                    // Stream was empty.  No point in parsing.
+                    return null;
 
                 }
                 forecastJsonStr = buffer.toString();
@@ -170,15 +175,15 @@ public class MainActivity extends ActionBarActivity {
 
             } finally {
                 if (urlConnection != null) {
-                urlConnection.disconnect();
+                    urlConnection.disconnect();
 
                 }
                 if (reader != null) {
-                try {
-                reader.close();
+                    try {
+                        reader.close();
 
                     } catch (final IOException e) {
-                Log.e("PlaceholderFragment", "Error closing stream", e);
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
 
                     }
 
