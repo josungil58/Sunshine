471da4a8f91e0e6a68b7a11fab3ab21e28bd90c6
josungil58 <josungil58@gmail.com>
2016-06-08 14:32:27 +0900

    4.01 input finished. No  change in the result.

--- Sunshine-Version-2-1.01_hello_world/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Sunshine-Version-2-1.01_hello_world/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends ActionBarActivity {
         //called to do initial creation of a fragment
         //Bundle에 저장된 Instance State를 불러옴
         //onCreate()는 protected method
+        Log.v(LOG_TAG, "in onCreate");
         super.onCreate(savedInstanceState);
         //Activity class의 onCreate()를 상속하였기 때문에 super.필요
         setContentView(R.layout.activity_main);
@@ -61,13 +62,13 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {//main.xml의 item
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
-        // Intent(Context packageContext, Class<?> cls)
-        // Context can be invoked using
-        // .getApplicationContext(), .getContext(), getBaseContext() or this(in the Activity Class)
-        // 'this' refers to the context of the current context
+            // Intent(Context packageContext, Class<?> cls)
+            // Context can be invoked using
+            // .getApplicationContext(), .getContext(), getBaseContext() or this(in the Activity Class)
+            // 'this' refers to the context of the current context
         }
 
-        if (id == R.id.action_map){
+        if (id == R.id.action_map) {
             openPreferenceLocaitonInMap();
             return true;
         }
@@ -76,41 +77,77 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-        private void openPreferenceLocaitonInMap(){
-            SharedPreferences sharedPrefs =
-                    PreferenceManager.getDefaultSharedPreferences(this);
-            // public interface SharedPreferences
-            // interface for accessing and modifying preference data returned by getSharedPreferences(String, int)
-            // public static SharedPreferences getDefaultSharedPreferences(Context context)
-            // 환경설정(settings)을 하기 위한 preference에서 저장된 data값을 활용
-            //
-            String location = sharedPrefs.getString(
-                    getString(R.string.pref_location_key),
-                    getString(R.string.pref_location_default));
+    private void openPreferenceLocaitonInMap() {
+        SharedPreferences sharedPrefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        // public interface SharedPreferences
+        // interface for accessing and modifying preference data returned by getSharedPreferences(String, int)
+        // public static SharedPreferences getDefaultSharedPreferences(Context context)
+        // 환경설정(settings)을 하기 위한 preference에서 저장된 data값을 활용
+        //
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
 
-            // Using the URI scheme for showing a location found on a map. This super-handy
-            // intent can is detailed in the "Common Intents" page of Android's developer site:
-            // http://developer.android.com/guide/component/intents.common.html#maps
-            // public abstract String getString(String key, String defValue)
+        // Using the URI scheme for showing a location found on a map. This super-handy
+        // intent can is detailed in the "Common Intents" page of Android's developer site:
+        // http://developer.android.com/guide/component/intents.common.html#maps
+        // public abstract String getString(String key, String defValue)
 
-            Uri geolocation = Uri.parse("geo:0,0?").buildUpon()
-                    .appendQueryParameter("q", location)
-                    .build();
-            // Maps intent에 전달되는 모든 문자열은 Uri로 encoding되어야 함.
-            // android.net.Uri.parse() method를 사용하여서 문자열을 encoding할 수 있음
-            // geo: latituce, longitude ?z=zoom; intent를 사용하여서 지도의 중심점을 설정
-            // z는 0(세계지도)부터 21(개별건물)까지 수준을 정할 수 있음.
-            // z값에 변화를 주어봤지만 별 차이가 없었음
+        Uri geolocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        // Maps intent에 전달되는 모든 문자열은 Uri로 encoding되어야 함.
+        // android.net.Uri.parse() method를 사용하여서 문자열을 encoding할 수 있음
+        // geo: latituce, longitude ?z=zoom; intent를 사용하여서 지도의 중심점을 설정
+        // z는 0(세계지도)부터 21(개별건물)까지 수준을 정할 수 있음.
+        // z값에 변화를 주어봤지만 별 차이가 없었음
 
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(geolocation);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geolocation);
 
-            if (intent.resolveActivity(getPackageManager()) != null) {
+        if (intent.resolveActivity(getPackageManager()) != null) {
             //  app이 intent를 수신할 수 있는지 확인, 결과가 null이 아니라는 소리는 intent를 처리할 수 있는
             //  앱이 적어도 하나는 있다는 말, null은 해당 intent를 사용해서는 안됨.에러 발생
-                startActivity(intent);
-            } else {
-                Log.d(LOG_TAG, "Couldn't call " + location + ". no receiving apps installed!");
-            }
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ". no receiving apps installed!");
         }
+    }
+
+    @Override
+    protected void onStart() {
+        Log.v(LOG_TAG, "in onStart");
+        super.onStart();
+        // The activity is about to become visible.
+    }
+
+    @Override
+    protected void onResume() {
+        Log.v(LOG_TAG, "in onResume");
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+
+    @Override
+    protected void onPause() {
+        Log.v(LOG_TAG, "in onPause");
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+
+    @Override
+    protected void onStop() {
+        Log.v(LOG_TAG, "in onStop");
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.v(LOG_TAG, "in onDestroy");
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+
 }
